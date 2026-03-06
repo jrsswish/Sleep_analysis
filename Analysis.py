02cb8da10f4e4f6257166d04be9fdc5498fe0967
jrsswish <quodet.johnrey@gmail.com>
2026-03-06 14:08:24 -0600

    Added categorical variables for Sleep_Hours, and implemented a box plot for the categorical variables

--- Analysis.py
+++ Analysis.py
@@ -2,24 +2,32 @@ import numpy as np
 import pandas as pd
 import sys
 import matplotlib.pyplot as plt
+import seaborn as sns
 import random
 
 def inputHandler(csvfile):
     df = pd.read_csv(csvfile)
-    df_sample = df.sample(n=50, random_state=1)
+    sleep = df['Sleep_Hours'].values
+    stress = df['Stress_Level'].values
+    sleep_category = []
+    n = len(sleep)
+    for i in range(n):
+        if sleep[i] < 5:
+            sleep_category.append("low")
+        elif sleep[i] > 5 and sleep[i] < 8:
+            sleep_category.append("norm")
+        elif sleep[i] > 8:
+            sleep_category.append("high")
+        else:
+            sleep_category.append("NA")
+    df['Sleep_Category'] = sleep_category
 
-    df_sorted = df_sample.sort_values(by='Stress_Level', ascending=True)
-
-    sample_sleep = list(df_sorted['Sleep_Hours'].values)
-    sample_stress = list(df_sorted['Stress_Level'].values)
-
-    xmin = min(sample_stress)
-    xmax = max(sample_stress)
-    plt.scatter( sample_stress, sample_sleep)
-    plt.ylabel('Sleep Hours')
-    plt.xlabel('Stress Level')
-    plt.xticks(np.arange(xmin, xmax, step=1))
+    print(df)
+    sns.boxplot(x='Sleep_Category', y='Stress_Level', data=df)
+    plt.xlabel("Sleep Hours")
+    plt.ylabel("Stress Level")
     plt.show()
+
 if __name__ == '__main__':
     csvfile = sys.argv[1]
     inputHandler(csvfile)
